0d5c361c2a198bef4e5ab4c69b19e58d13185820
CesarRivasP <cesarjose58@gmail.com>
2019-04-27 17:43:51 -0400

    Create function attendTicket

--- server/classes/ticket-control.js
+++ server/classes/ticket-control.js
@@ -2,9 +2,9 @@ const fs = require('fs');
 
 // Para manejar los tickets pendientes
 class Ticket {
-  constructor(number, box){ //numero del ticket que se va a atender, que caja lo va a atender
+  constructor(number, numberBox){ //numero del ticket que se va a atender, que caja lo va a atender
     this.number = number;
-    this.box = box;
+    this.numberBox = numberBox;
   }
 }
 
@@ -17,6 +17,7 @@ class TicketControl {
     this.today = new Date().getDate();
     // Va a contener todos los tickets pendientes de revision
     this.tickets = [];  //todos los tickets que no han sido atendidos
+    this.lastFour = []; //ultimos 4 tickets que se estan atendiendo
 
     let data = require('../data/data.json');
 
@@ -28,6 +29,7 @@ class TicketControl {
       this.last = data.last;
 
       this.tickets = data.tickets;
+      this.lastFour = data.lastFour;
     }
     else {
       this.restartCounting()
@@ -52,12 +54,43 @@ class TicketControl {
     return`Ticket ${this.last}`;
   }
 
+  attendTicket(numberBox){
+    // si no hay tickets pendientes, no se debe hacer nada
+    if(this.tickets.length === 0){
+      return 'No hay tickets'
+    }
+    // Hay que tomar el primer ticket que se encuentre en el array de tickets
+    let numberTicket = this.tickets[0].number;
+    // Una vez tomado dicho ticket, se debe borrar el mismo del array
+    this.tickets.shift(); //Asi se elimina el primer elemento
+
+    // Instancia de un nuevo ticket, puesto que este es el que se quiere atender
+    let attendTicket = new Ticket(numberTicket, numberBox); //Creacion del nuevo ticket que se va a atender
+    // Se va a agregar el nuevo ticket al inicio del array
+    this.lastFour.unshift(attendTicket); //unshift para agregarlo al inicio del array
+
+    // Cuando el array se llene se deben borrar los primeros que ingresaron en orden
+    if(this.lastFour.length > 4){
+      // Para remover el ultimo elemento
+      this.lastFour.splice(-1,1);
+    }
+
+    console.log('Ultimos 4');
+    console.log(this.lastFour);
+
+    this.recordFile();
+
+    // ticket a atender
+    return attendTicket;
+  }
+
   recordFile(){
     let jsonData = {
       last: this.last,
       today: this.today,
       // tambien hay que grabar los tickets pendientes
-      tickets: this.tickets //asi se graban los tickets que esten pendientes por atender
+      tickets: this.tickets, //asi se graban los tickets que esten pendientes por atender
+      lastFour: this.lastFour
     };
 
     let jsonDataString = JSON.stringify(jsonData);
@@ -70,6 +103,7 @@ class TicketControl {
     this.last = 0;
     // Aparte de reiniciar cual fue el ultimo, se debe reiniciar todo lo que contiene el objeto ticket
     this.tickets = [];
+    this.lastFour = [];
     this.recordFile();
 
     console.log('se ha inicializado el sistema');
